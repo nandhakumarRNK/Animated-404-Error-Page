353606100820cdaff703de2e629f1645747fe8a3
RNK Nandhakumar <121867811+nandhakumarRNK@users.noreply.github.com>
2023-02-23 10:09:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Animated 404 Error Page
+
+### Responsive Animated 404 Error Page
+
+- Simple Animated 404 Error Page Using React JS & GIF
+
+![preview img](/preview.gif)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ErrorPageContainer from "./ErrorPageContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorPageContainer />
+    </div>
+  );
+}
+
+export default App;
